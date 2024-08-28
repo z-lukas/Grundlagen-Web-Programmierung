1069c82662f0262150429c5a2b935328bd5db6da
Lukas Zumstein <lukas.zumstein@redq.ch>
2024-08-28 13:28:36 +0200

    mod server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Korrigiert den Operator von "|" zu "||"
 
 // Middleware, um JSON-Daten aus dem Request-Body zu parsen
 app.use(bodyParser.json());
+
+// Middleware, um statische Dateien bereitzustellen
+app.use(express.static(path.join(__dirname, 'public'))); // Setzt voraus, dass deine statischen Dateien im "public"-Ordner liegen
+
+// Route für die Startseite
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Route, um POST-Anfragen vom Kontaktformular zu empfangen
 app.post('/send-message', (req, res) => {
