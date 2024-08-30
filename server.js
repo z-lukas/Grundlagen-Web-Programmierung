b3b4b2e98da32f50166d7515c03e83daa94feb41
Lukas Zumstein <lukas.zumstein@redq.ch>
2024-08-30 16:32:20 +0200

    done

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000; // Korrigiert den Operator von "|" zu "||"
+const port = process.env.PORT || 3000; 
 
 // Middleware, um JSON-Daten aus dem Request-Body zu parsen
 app.use(bodyParser.json());
 
 // Middleware, um statische Dateien bereitzustellen
-app.use(express.static(path.join(__dirname, 'public'))); // Setzt voraus, dass deine statischen Dateien im "public"-Ordner liegen
-
+app.use(express.static(path.join(__dirname, 'public'))); 
 // Route für die Startseite
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -19,8 +18,6 @@ app.get('/', (req, res) => {
 app.post('/send-message', (req, res) => {
     const { name, email, message } = req.body;
     console.log(`Nachricht erhalten von ${name} (${email}): ${message}`);
-    
-    // Hier könntest du die Daten z.B. in einer Datenbank speichern oder eine E-Mail senden
 
     res.json({ success: true });
 });
